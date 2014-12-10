c2ca5e25a3e16ff82754817dd4dc60c76fa527de
Alex Browne <stephenalexbrowne@gmail.com>
2014-12-10 17:13:47 -0500

    Add some comments

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	base.Silent()
+	// Load and parse the data from csv files
 	fmt.Println("Loading data...")
 	trainData, err := base.ParseCSVToInstances("data/mnist_train.csv", true)
 	if err != nil {
@@ -19,19 +19,27 @@ func main() {
 		panic(err)
 	}
 
+	// Create a new linear SVC with some good default values
 	classifier, err := linear_models.NewLinearSVC("l1", "l2", true, 1.0, 1e-4)
 	if err != nil {
 		panic(err)
 	}
+
+	// Don't output information on each iteration
+	base.Silent()
+
+	// Train the linear SVC
 	fmt.Println("Training...")
 	classifier.Fit(trainData)
 
+	// Make predictions for the test data
 	fmt.Println("Predicting...")
 	predictions, err := classifier.Predict(testData)
 	if err != nil {
 		panic(err)
 	}
 
+	// Get a confusion matrix and print out some accuracy stats for our predictions
 	confusionMat, err := evaluation.GetConfusionMatrix(testData, predictions)
 	if err != nil {
 		panic(fmt.Sprintf("Unable to get confusion matrix: %s", err.Error()))
